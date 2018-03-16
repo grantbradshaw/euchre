eafd79d8207460dec91154e059ea6dc914b66a1c
Grant Bradshaw <grant.james.bradshaw@gmail.com>
2018-03-16 14:09:38 -0700

    Minor refactoring in main file.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # master filing for launching application
 
 from objects import game, team, player, card
-from environment import HEARTS, DIAMONDS, SPADES, CLUBS, A, K, Q, J, TEN, NINE, 
-						SUITS, SYMBOLS
+from environment import HEARTS, DIAMONDS, SPADES, CLUBS, A, K, Q, J, TEN, NINE, SUITS, SYMBOLS
 
 # master function to start a game of Euchre
 def run():
